d92aae21a6e29d36dfb4a930c7a41c439c43f347
aymen <samoudiayman777@gmail.com>
2024-04-11 19:44:54 +0000

    Refactor number_of_subscribers function in 0-subs.py to improve error handling and remove unnecessary code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
-
 """Function to query subscribers on a given Reddit subreddit."""
-
 import requests
+
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        try:
-            results = response.json().get("data")
-            if results:
-                return results.get("subscribers", 0)
-            else:
-                return 0
-        except (KeyError, TypeError):
-            # Handle the case where the response data is not in the expected format
-            return 0
-    elif response.status_code == 404:
-        # Handle the case where the subreddit doesn't exist
+    if response.status_code == 404:
         return 0
-    else:
-        # Handle other response status codes
-        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
