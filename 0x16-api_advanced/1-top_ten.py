626128ad9ec885c144e4022e8c158bcc74ae1e91
aymen <samoudiayman777@gmail.com>
2024-04-11 20:28:33 +0000

    Refactor top_ten function in 1-top_ten.py to improve error handling and use try-except block for requests

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,10 +12,13 @@ def top_ten(subreddit):
     params = {
         "limit": 10
     }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    try:
+        response = requests.get(url, headers=headers, params=params,
+                                allow_redirects=False)
+        if response.status_code == 404:
+            print("None")
+            return
+        results = response.json().get("data")
+        [print(c.get("data").get("title")) for c in results.get("children")]
+    except requests.exceptions.RequestException as e:
+        print("An error occurred:", e)
